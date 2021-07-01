2d28345f90ed6d4a8bb7ea6c16ede2bfe7748dcf
Evon Perez <perez.evon@gmail.com>
2021-07-01 18:59:02 -0400

    cleaned up some code, added some html ele

--- app.js
+++ app.js
@@ -2,10 +2,7 @@
 import { randomItem, randomNum, compareItems } from './utils.js';
 // initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// update DOM to reflect the new state
 
 const play_btn = document.getElementById('play-btn');
 const wins_section = document.getElementById('wins');
@@ -13,28 +10,18 @@ const losses_section = document.getElementById('losses');
 const draws_section = document.getElementById('draws');
 const compare_section = document.getElementById('compare-section');
 
-
 let wins = 0;
 let losses = 0;
 let draws = 0; 
-
-
-
 
 play_btn.addEventListener('click', () => {
 
     const user_selected_item = document.querySelector('input[type=radio]:checked');
     const user_item = user_selected_item.value;
-
-
-    let comp_item = randomItem(randomNum());
-//console.log(comp_item);
-
-    let result = compareItems(user_item, comp_item);
-
+    const comp_item = randomItem(randomNum());
+    const result = compareItems(user_item, comp_item);
 
     compare_section.innerText = `You chose ${user_item} and the computer chose ${comp_item}`;
-
 
     if (result === 'win'){
 
@@ -54,18 +41,5 @@ play_btn.addEventListener('click', () => {
         draws_section.innerText = `You had ${draws} draws`;
 
     }
-
-    console.log(result);
-
-
-
-
-
-
-
-
-//random item selection function for computer
-
-
 
 });
--- utils.js
+++ utils.js
@@ -2,8 +2,6 @@ export function compareItems(user_item, computer_item){
 
     let str = '';
 
-    
-
     if (user_item === computer_item){
 
         str = 'draw';
@@ -13,44 +11,32 @@ export function compareItems(user_item, computer_item){
 
         str = 'win';
 
-
     }
-
     
     else if (user_item === 'rock' && computer_item === 'scissors'){
 
         str = 'win';
 
-
     }
-
     
     else if (user_item === 'scissors' && computer_item === 'paper'){
 
-        
         str = 'win';
-
     }
 
     else {
 
-
         str = 'lose';
-
     }
 
     return str;
-
-
 }
 
 
 
 export function randomNum(){
 
-
     return Math.floor(Math.random() * 3) + 1;
-
 
 }
 
@@ -60,20 +46,14 @@ export function randomItem(num){
     let str = '';
 
     if (num === 1){
-
         str = 'rock';
     }
-
     if (num === 2){
-
         str = 'paper';
     }
-
     if (num === 3){
-
         str = 'scissors';
     }
-
     return str;
 
 }
